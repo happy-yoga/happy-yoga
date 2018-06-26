89fa2bbc7e90909421efeee388400710399908c8
Falk Hoppe <falkhoppe81@gmail.com>
2018-06-26 13:53:25 +0200

    add simple html rendering

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ const express = require('express')
 
 const morgan = require('morgan')
 const helmet = require('helmet')
+const pug = require('pug')
 
 const app = express()
 
 app.use(morgan())
 app.use(helmet())
+
+app.set('view engine', 'pug')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
 
 app.listen(process.env.PORT, () => {
   console.info(`application server started at port: ${process.env.PORT}`)

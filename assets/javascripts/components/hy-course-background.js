e023b0c489e2ca11dfb5ccd825223600d09d767d
Falk Hoppe <falkhoppe81@gmail.com>
2018-07-14 14:03:55 +0200

    improve bg item distribution

--- assets/javascripts/components/hy-course-background.js
+++ assets/javascripts/components/hy-course-background.js
@@ -18,11 +18,13 @@ class HyCourseBackground extends HTMLElement {
 
     item.addClass(type)
     item.attr('data-rellax-speed', -1 * Math.ceil(Math.random() * 5))
-    item.css('margin-left', `${Math.ceil(Math.random() * 59)}vw`)
+    item.css('margin-left', `${Math.ceil(Math.random() * 20) * Math.ceil(Math.random() * 5)}vw`)
     item.css('margin-top', `${Math.ceil(Math.random() * 10)}vh`)
 
     if (type === 'ball') {
       item.css('font-size', `${Math.floor(Math.random() * 2) + 1}rem`)
+    } else {
+      item.css('height', `${Math.ceil(Math.random() * 20) + 20}rem`)
     }
 
     this.$.append(item)

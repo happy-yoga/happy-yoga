391ac4273ab8f9dcc4bac5548a196e6c80be504c
Falk Hoppe <falkhoppe81@gmail.com>
2018-07-14 14:32:53 +0200

    further improvement

--- data/courses.js
+++ data/courses.js
@@ -1,29 +1,29 @@
 module.exports = {
   monday: [
-    { title: 'Gentle Yoga', teacher: 'Claudia', hint: 'sanft' },
-    { title: 'Yoga Flow', teacher: 'Julia', hint: 'Level II' },
-    { title: 'Yoga Basics', teacher: 'Julia', hint: 'Level 0' }
+    { title: 'Gentle Yoga', teacher: 'Claudia', hint: 'sanft', begin: '10:00', end: '11:30' },
+    { title: 'Yoga Flow', teacher: 'Julia', hint: 'Level II', begin: '18:15', end: '19:30' },
+    { title: 'Yoga Basics', teacher: 'Julia', hint: 'Level 0', begin: '20:00', end: '21:30' }
   ],
   tuesday: [
-    { title: 'Happy Devis', teacher: 'Kerstin', hint: 'Schwangere' },
-    { title: 'Slow Flow', teacher: 'Julia', hint: 'Level I-II' },
-    { title: 'Happy Yoga Routine', teacher: 'Julia', hint: 'Level 0-I' }
+    { title: 'Happy Devis', teacher: 'Kerstin', hint: 'Schwangere', begin: '10:30', end: '12:00' },
+    { title: 'Slow Flow', teacher: 'Julia', hint: 'Level I-II', begin: '18:15', end: '19:30' },
+    { title: 'Happy Yoga Routine', teacher: 'Julia', hint: 'Level 0-I', begin: '20:00', end: '21:30' }
   ],
   wednesday: [
-    { title: 'Yoga für Senioren', teacher: 'Claudia' },
-    { title: 'Starker Rücken', teacher: 'Julia', hint: 'Level I-II' },
-    { title: 'Luna Yoga', teacher: 'Claudia', hint: 'sanft' }
+    { title: 'Yoga für Senioren', teacher: 'Claudia', begin: '16:30', end: '17:30' },
+    { title: 'Starker Rücken', teacher: 'Julia', hint: 'Level I-II', begin: '18:15', end: '19:40' },
+    { title: 'Luna Yoga', teacher: 'Claudia', hint: 'sanft', begin: '20:00', end: '21:30' }
   ],
   thursday: [
-    { title: 'Morning Yoga auf Zollverein', teacher: 'Julia' },
-    { title: 'Happy Baby & Mom', teacher: 'Kerstin', hint: 'Postnatal' },
-    { title: 'Yoga Basics', teacher: 'Susi', hint: 'Level I' },
-    { title: 'Happy Devis', teacher: 'Kerstin', hint: 'Schwangere' }
+    { title: 'Morning Yoga auf Zollverein', teacher: 'Julia', begin: '09:00', end: '10:00' },
+    { title: 'Happy Baby & Mom', teacher: 'Kerstin', hint: 'Postnatal', begin: '10:00', end: '11:00' },
+    { title: 'Yoga Basics', teacher: 'Susi', hint: 'Level I', begin: '18:15', end: '19:30' },
+    { title: 'Happy Devis', teacher: 'Kerstin', hint: 'Schwangere', begin: '20:00', end: '21:30' }
   ],
   friday: [
-    { title: 'Yoga Flow', teacher: 'Julia', hint: 'Level I-II' },
-    { title: 'Yin & Faszienmassage', teacher: 'Tine', hint: 'alle Level' }
+    { title: 'Yoga Flow', teacher: 'Julia', hint: 'Level I-II', begin: '09:00', end: '10:30' },
+    { title: 'Yin & Faszienmassage', teacher: 'Tine', hint: 'alle Level', begin: '18:30', end: '20:00' }
   ],
   saturday: [],
-  sunday: [{ title: 'Yoga Flow', teacher: 'Kirsten', hint: 'Level I-II' }]
+  sunday: [{ title: 'Yoga Flow', teacher: 'Kirsten', hint: 'Level I-II', begin: '18:00', end: '19:30' }]
 }

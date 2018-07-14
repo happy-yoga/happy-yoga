0152bf984a10ef8dbceaa3aa7f5f91257087b102
Falk Hoppe <falkhoppe81@gmail.com>
2018-07-14 12:37:39 +0200

    add language stack

---
+++ data/courses.js
@@ -0,0 +1,29 @@
+module.exports = {
+  monday: [
+    { title: 'Gentle Yoga', teacher: 'Claudia', hint: 'sanft' },
+    { title: 'Yoga Flow', teacher: 'Julia', hint: 'Level II' },
+    { title: 'Yoga Basics', teacher: 'Julia', hint: 'Level 0' }
+  ],
+  tuesday: [
+    { title: 'Happy Devis', teacher: 'Kerstin', hint: 'Schwangere' },
+    { title: 'Slow Flow', teacher: 'Julia', hint: 'Level I-II' },
+    { title: 'Happy Yoga Routine', teacher: 'Julia', hint: 'Level 0-I' }
+  ],
+  wednesday: [
+    { title: 'Yoga für Senioren', teacher: 'Claudia' },
+    { title: 'Starker Rücken', teacher: 'Julia', hint: 'Level I-II' },
+    { title: 'Luna Yoga', teacher: 'Claudia', hint: 'sanft' }
+  ],
+  thursday: [
+    { title: 'Morning Yoga auf Zollverein', teacher: 'Julia' },
+    { title: 'Happy Baby & Mom', teacher: 'Kerstin', hint: 'Postnatal' },
+    { title: 'Yoga Basics', teacher: 'Susi', hint: 'Level I' },
+    { title: 'Happy Devis', teacher: 'Kerstin', hint: 'Schwangere' }
+  ],
+  friday: [
+    { title: 'Yoga Flow', teacher: 'Julia', hint: 'Level I-II' },
+    { title: 'Yin & Faszienmassage', teacher: 'Tine', hint: 'alle Level' }
+  ],
+  saturday: [],
+  sunday: [{ title: 'Yoga Flow', teacher: 'Kirsten', hint: 'Level I-II' }]
+}
--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 require('dotenv').config()
+const path = require('path')
 const express = require('express')
 
 const morgan = require('morgan')
 const helmet = require('helmet')
 const pug = require('pug')
+const i18n = require('i18next')
+const i18nMiddleware = require('i18next-express-middleware')
+const i18nBackend = require('i18next-node-fs-backend')
+
+const supportedLanguages = ['de', 'en']
+
+i18n
+  .use(i18nMiddleware.LanguageDetector)
+  .use(i18nBackend)
+  .init({
+    preload: ['de'],
+    backend: {
+      // TODO: when entering a new language, this has to be dynamised
+      loadPath: path.resolve(__dirname, 'locales/{{lng}}/main.yaml')
+    }
+  })
+
+const courses = require('./data/courses.js')
 
 const app = express()
+app.enable('strict routing')
+
+app.use(
+  i18nMiddleware.handle(i18n, {
+    removeLngFromUrl: false
+  })
+)
 
 app.use(morgan('common'))
 app.use(helmet())
@@ -17,9 +43,26 @@ app.use('/assets/images', express.static('dist/images'))
 app.use('/assets/javascripts', express.static('dist/javascripts'))
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.redirect(`/${preferredLanguage(req)}/`)
+})
+
+app.get('/:lang', (req, res) => {
+  res.redirect(`/${req.params.lang}/`)
+})
+
+app.get('/:lang/', (req, res) => {
+  console.log('LANGUAGE', req.params.lang)
+  req.i18n.changeLanguage(req.params.lang)
+  res.render('index', { i18n: req.i18n, courses })
 })
 
 app.listen(process.env.PORT, () => {
   console.info(`application server started at port: ${process.env.PORT}`)
 })
+
+function preferredLanguage (req) {
+  const fallback = 'de'
+  const preferred = req.languages.reduce((curr, next) => (supportedLanguages.indexOf(curr) >= 0 ? curr : next))
+
+  return supportedLanguages.indexOf(preferred) >= 0 ? preferred : fallback
+}

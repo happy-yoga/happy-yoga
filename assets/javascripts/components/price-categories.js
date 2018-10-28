54a7ded26b78bb79b973bf70c78c0e38a6311cef
Falk Hoppe <falkhoppe81@gmail.com>
2018-10-28 13:16:54 +0100

    ns

--- assets/javascripts/components/price-categories.js
+++ assets/javascripts/components/price-categories.js
@@ -1,42 +1,11 @@
-/* global customElements, HTMLElement, Swiper */
+// /* global window, customElements, HTMLElement */
 
-class PriceCategories extends HTMLElement {
-  constructor (self) {
-    self = super(self)
-    return self
-  }
-
-  connectedCallback () {
-    const _ = new Swiper(this, {
-      // Optional parameters
-      direction: 'horizontal',
-      loop: true,
-      autoHeight: true,
-      slidesPerView: 1,
-      keyboard: {
-        enabled: true,
-        onlyInViewport: false
-      },
-
-      // If we need pagination
-      pagination: {
-        el: '.swiper-pagination'
-      },
-
-      // Navigation arrows
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-      },
-
-      // And if we need scrollbar
-      scrollbar: {
-        el: '.swiper-scrollbar'
-      }
-    })
-
-    return _
-  }
-}
-
-customElements.define('price-categories', PriceCategories)
+// class PriceCategories extends HTMLElement {
+//   constructor (self) {
+//     self = super(self)
+//
+//     return self
+//   }
+// }
+//
+// customElements.define('price-categories', PriceCategories)

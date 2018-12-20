dd9e78e8718aa788d279030252a9ee60fb1c8249
Falk Hoppe <falkhoppe81@gmail.com>
2018-12-20 17:24:27 +0100

    add dynamic price list

--- assets/javascripts/components/price-categories.js
+++ assets/javascripts/components/price-categories.js
@@ -1,11 +1,45 @@
-// /* global window, customElements, HTMLElement */
+/* global window, customElements, HTMLElement, $ */
 
-// class PriceCategories extends HTMLElement {
-//   constructor (self) {
-//     self = super(self)
-//
-//     return self
-//   }
-// }
-//
-// customElements.define('price-categories', PriceCategories)
+const indicator = $('<price-category-opening-indicator />')
+indicator.hide()
+
+const openClass = 'open'
+const activeClass = 'active'
+class PriceCategories extends HTMLElement {
+  constructor (self) {
+    self = super(self)
+    return self
+  }
+
+  connectedCallback () {
+    this.$ = $(this)
+    this.priceCategories = $('price-category', this.$)
+
+    this.priceCategories.on('click', e => {
+      e.preventDefault()
+
+      const priceCategory = $(e.currentTarget)
+      const isActive = priceCategory.hasClass(activeClass)
+
+      window.requestAnimationFrame(() => {
+        this.priceCategories.removeClass(activeClass)
+        window.requestAnimationFrame(() => {
+          this.priceCategories.removeClass(openClass)
+
+          window.requestAnimationFrame(() => {
+            if (!isActive) {
+              window.requestAnimationFrame(() => {
+                priceCategory.addClass(openClass)
+                window.requestAnimationFrame(() => {
+                  priceCategory.addClass(activeClass)
+                })
+              })
+            }
+          })
+        })
+      })
+    })
+  }
+}
+
+customElements.define('price-categories', PriceCategories)
